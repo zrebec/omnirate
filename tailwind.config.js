a84b689e65d92e60471c60d12c687fbbe6a6ae02
zrebec <zrebec@zrebec.sk>
2021-04-17 04:12:08 +0200

    prettierrc, tailwind config, production styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   purge: {
     enabled: false,
-    content: [
-      './dist/**/*.html',
-      './src/**/*.js'
-    ]
+    content: ["./dist/**/*.html", "./src/**/*.js"],
   },
   darkMode: false, // or 'media' or 'class'
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
