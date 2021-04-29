43a21b9c456e556a15a8f26ff16ab715ef10aed4
zrebec <zrebec@zrebec.sk>
2021-04-29 22:03:31 +0200

    Grid auto-fit, cosmetic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ["./dist/**/*.html", "./src/**/*.js"],
+  },
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+    },
+    variants: {},
+    plugins: [],
   },
   darkMode: false, // or 'media' or 'class'
   variants: {
